cba25fb8dc660db3d10dbfce4180754367e27fd1
Alexei Sarmiento Rivas <verosoft@gmail.com>
2025-11-30 21:37:56 +0100

    feat(todo): Implement full CRUD and progress tracking for Todo Items
    This commit introduces comprehensive management functionalities for todo items in the web application:
    - Added the ability to create new todo items.
    - Implemented functionality to edit existing todo item descriptions.
    - Integrated progress tracking for todo items with a visual progress bar.
    - Enhanced the UI with dialogs for adding, editing, and updating progress.

--- BeyondTodoWebApp/beyond-todo-webapp/pages/index.vue
+++ BeyondTodoWebApp/beyond-todo-webapp/pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="ma-16">
-    <p class="text-h6">Todo Items Admin</p>
     <div>
       <div v-if="pending">
         Cargando...
@@ -15,11 +14,34 @@
         <v-data-table-server :v-model:items-per-page="itemsPerPage" :headers="headers" :items="data?.data"
           :items-length="totalItems">
 
+          <template v-slot:top>
+            <v-toolbar flat>
+              <v-toolbar-title>
+                <v-icon color="medium-emphasis" icon="mdi-format-list-checks" size="x-small" start></v-icon>
+                Todo Items Admin
+              </v-toolbar-title>
+              <v-btn class="me-2" prepend-icon="mdi-plus" rounded="lg" text="Add Item" border
+                @click="loadNewDialog"></v-btn>
+            </v-toolbar>
+          </template>
+
+          <template v-slot:item.currentPercent="{ item }">
+            <v-progress-linear :model-value="item.currentPercent" :color="getProgressColor(item.currentPercent)"
+              height="20" striped>
+              <template v-slot:default="{ value }">
+                <strong>{{ value }}%</strong>
+              </template>
+            </v-progress-linear>
+          </template>
+
           <template v-slot:item.actions="{ item }">
             <div class="d-flex ga-2 justify-end">
-              <v-icon color="medium-emphasis" icon="mdi-pencil" size="small" @click="edit(item.id)"></v-icon>
+              <v-icon color="medium-emphasis" icon="mdi-pencil" size="small" @click="loadEditDailog(item.id)"></v-icon>
 
               <v-icon color="medium-emphasis" icon="mdi-delete" size="small" @click="remove(item.id)"></v-icon>
+
+              <v-icon color="medium-emphasis" icon="mdi-progress-upload" size="small"
+                @click="loadProgressDialog(item.id)"></v-icon>
             </div>
           </template>
 
@@ -27,7 +49,26 @@
       </ul>
     </div>
 
-
+    <v-dialog v-model="editDialog" max-width="500">
+      <v-card :title="`${isEdit ? 'Edit Description' : isNew ? 'New Todo' : 'Update Percent'}`">
+        <v-card-text>
+          <v-text-field label="New Description" v-model="editDescription" v-if="isEdit"></v-text-field>
+          <div v-else-if="isNew">
+            <v-text-field label="Title" v-model="newTitle"></v-text-field>
+            <v-text-field label="Description" v-model="newDescription"></v-text-field>
+            <v-combobox label="Category" :items="['Work', 'Personal', 'Hobby']" v-model="newCategory"></v-combobox>
+          </div>
+          <v-number-input :max="99" :min="1" v-model="percent" v-else></v-number-input>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn text="Save" v-if="isEdit" @click="edit"></v-btn>
+          <v-btn text="New" v-else-if="isNew" @click="newItem"></v-btn>
+          <v-btn text="Add Percent" v-else @click="addPercent"></v-btn>
+          <v-btn text="Close" @click="editDialog = false"></v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
   </div>
 </template>
@@ -40,6 +81,13 @@ interface TodoItem {
   description: string;
   category: string;
   isCompleted: boolean;
+  progressions: Progessions[],
+  currentPercent: number
+}
+
+interface Progessions {
+  date: Date,
+  percent: number
 }
 
 interface TodoResponse {
@@ -68,10 +116,131 @@ const headers = ref([
   { title: 'Title', key: 'title', align: 'start' as const, sortable: true },
   { title: 'Description', key: 'description', align: 'start' as const, sortable: true },
   { title: 'Category', key: 'category', align: 'start' as const, sortable: true },
+  { title: 'Progress', key: 'currentPercent', align: 'start' as const, sortable: true },
   { title: 'Completed', key: 'isCompleted', align: 'start' as const, sortable: true },
   { title: 'Action', key: 'actions', align: 'end' as const, sortable: false },
-
 ]);
+
+const getProgressColor = (percent: number) => {
+  if (percent === 100) {
+    return 'success';
+  } else if (percent > 50) {
+    return 'info';
+  } else if (percent > 0) {
+    return 'warning';
+  } else {
+    return 'error';
+  }
+};
+
+const editDescription = ref('');
+const editDialog = ref(false);
+const editId = ref<number>();
+const isEdit = ref<boolean>(false);
+const isNew = ref<boolean>(false);
+const percent = ref<number>(1);
+
+const newTitle = ref('');
+const newDescription = ref('');
+const newCategory = ref('');
+
+
+const loadNewDialog = () => {
+  newTitle.value = '';
+  newDescription.value = '';
+  newCategory.value = '';
+  isEdit.value = false;
+  isNew.value = true;
+  editDialog.value = true;
+}
+
+const newItem = async () => {
+
+  const apiResponse = await $fetch<ApiResponseBody>(`/todos/`, {
+    method: 'POST',
+    baseURL: base_url,
+    body: {
+      title: newTitle.value,
+      description: newDescription.value,
+      category: newCategory.value
+
+    }
+  })
+
+  const { success, message, data } = apiResponse;
+  console.log(message);
+
+  editDialog.value = false;
+  isEdit.value = false;
+  isNew.value = false;
+  
+  newTitle.value = '';
+  newDescription.value = '';
+  newCategory.value = '';
+
+  refresh();
+}
+
+const loadProgressDialog = (id: number) => {
+  isEdit.value = false;
+  isNew.value = false;
+  editDialog.value = true;
+  editId.value = id;
+}
+
+const addPercent = async () => {
+
+  const now = new Date();
+  const utcDateTime = now.toISOString();
+
+  const apiResponse = await $fetch<ApiResponseBody>(`/todos/${editId.value}/progress`, {
+    method: 'PATCH',
+    baseURL: base_url,
+    body: {
+      dateTime: utcDateTime,
+      percent: percent.value
+    }
+  })
+
+  const { success, message, data } = apiResponse;
+  console.log(message);
+
+  editDialog.value = false;
+  isEdit.value = false;
+  isNew.value = false;
+  percent.value = 1;
+
+  refresh();
+}
+
+
+
+const loadEditDailog = (id: number) => {
+  isEdit.value = true;
+  isNew.value = false;
+  editDescription.value = '';
+  editDialog.value = true;
+  editId.value = id;
+}
+
+const edit = async () => {
+  const apiResponse = await $fetch<ApiResponseBody>(`/todos/${editId.value}/description`, {
+    method: 'PATCH',
+    baseURL: base_url,
+    body: {
+      newDescription: editDescription.value
+    }
+  })
+
+  const { success, message, data } = apiResponse;
+  console.log(message);
+
+  editDialog.value = false;
+  isEdit.value = false;
+  isNew.value = false;
+
+  refresh();
+}
 
 const remove = async (id: number) => {
   try {
@@ -81,11 +250,12 @@ const remove = async (id: number) => {
     });
 
     const { success, message, data } = apiResponse;
-
     console.log(message);
 
     refresh();
+
   } catch (error) {
+
     if (error && typeof error === 'object' && 'response' in error) {
 
       const response = (error as any).response;
@@ -103,16 +273,13 @@ const remove = async (id: number) => {
             console.error(`Error no manejado: ${statusCode}`);
             break;
         }
-
       }
-
-
     } else {
-     
       console.error('Error no HTTP/de red:', error);
     }
+
   }
-};
+}
 
 </script>
 
